1736e8f71903f531bfb4666057b682d4ec0dda13
Dritriy Golyshev <4dimariK@gmail.com>
2022-03-31 21:10:58 +0300

    7. Создание новых данных

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
+import axios from "axios"
 
 const EditQualityPage = () => {
+    const [quality, setQuality] = useState(null);
+    const {id} = useParams()
+    const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
+    const handeleSubmit = (data)=>{
+        axios.put(qualityEndPoint, data).then(res=>console.log(res.data.content))
+    }
+
+    useEffect(async ()=>{
+        const {data} = await axios.get(qualityEndPoint)
+        setQuality(data.content)
+    },[id]);
     return (
         <>
-            <h1>Edit Quality Page</h1> <EditForm />
+            <h1>Edit Quality Page</h1> 
+            {quality!==null ? <EditForm data={quality} onSubmit={handeleSubmit} />:"Loading"}
         </>
     );
 };
--- src/app/pages/qualititesList.jsx
+++ src/app/pages/qualititesList.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import QualitiesTable from "../components/ui/qualitiesTable";
+import axios from "axios"
+
+
 
 const QualitiesListPage = () => {
+    const [qualities, setQualities] = useState([]);
     const history = useHistory();
+
+    useEffect(async ()=>{
+        const {data} = await axios.get("http://localhost:4000/api/v1/quality")
+        setQualities(data.content)
+    },[]);
+
     const handleEdit = (param) => {
-        console.log(param);
         history.push(`/edit/${param}`);
     };
     const handleDelete = (param) => {
@@ -17,7 +26,7 @@ const QualitiesListPage = () => {
             <QualitiesTable
                 onDelete={handleDelete}
                 onEdit={handleEdit}
-                data={[]}
+                data={qualities}
             />
         </>
     );
